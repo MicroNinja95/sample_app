2de7ba910e95eb1b4c9c79ef537d081eef3a1b74
MicroNinja95 <brianna.yagow@siu.edu>
2017-11-30 12:29:11 -0600

    Finished layout and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-<<<<<<< HEAD
-=======
+
   def hello
     render text: "hello, world!"
   end
->>>>>>> 24af28e700ce703ea80fc9310ff1baff66258da8
+
 end
